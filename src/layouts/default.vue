66e1a274d6c84d81a03f59a98e2cb8f0212933e2
Supawite Kiatrattanakorn <supawite08@gmail.com>
2024-11-16 18:06:19 +0700

    Add default navigation bar layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,32 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      expand-on-hover
+      :mini-variant="mini"
+      permanent
+      rail
+    >
+      <v-list density="compact" nav>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :prepend-icon="item.icon"
+          :title="item.title"
+          :to="item.to"
+        />
+      </v-list>
+
+      <template #append>
+        <v-list-item
+          nav
+          prepend-icon="mdi-logout"
+          title="Logout"
+          @click="useAuthStore().logout()"
+        />
+      </template>
+    </v-navigation-drawer>
+
     <v-main>
       <router-view />
     </v-main>
@@ -7,5 +34,12 @@
 </template>
 
 <script lang="ts" setup>
-  //
+  import { ref } from 'vue'
+  import { useAuthStore } from '../stores/auth'
+
+  const items = ref([
+    { title: 'Home', icon: 'mdi-view-dashboard', to: '/home' },
+    { title: 'Placeholder', icon: 'mdi-image', to: '/placeholder' },
+  ])
+  const mini = ref(true)
 </script>
