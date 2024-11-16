1ea1978dc860073f5d9feb52533065b037ef3256
Supawite Kiatrattanakorn <supawite08@gmail.com>
2024-11-16 22:29:11 +0700

    Add home template (Fetch Device)

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <div class="text-h3 text-center mb-8 font-weight-bold inline">Welcome!</div>
-  </div>
+  <v-sheet class="fill-height bg-grey-darken-3">
+    <div class="d-flex flex-wrap pa-6 ga-4">
+      <v-card>
+        <v-card-title>Total Devices</v-card-title>
+        <v-card-text>{{ devicesCount }}</v-card-text>
+      </v-card>
+      <v-card>
+        <v-card-title>Total Topics</v-card-title>
+        <v-card-text>{{ topicsCount }}</v-card-text>
+      </v-card>
+    </div>
+  </v-sheet>
 </template>
 
 <script lang="ts" setup>
-  //
+  import { ref } from 'vue'
+  import { fetchWrapper } from '@/helper/fetch-wrapper'
+
+  const devicesCount = ref(0)
+  const topicsCount = ref(0)
+
+  onMounted(() => {
+    getDevices()
+  })
+
+  const getDevices = async () => {
+    devicesCount.value = 0
+    topicsCount.value = 0
+    const response = await fetchWrapper.get(`/api/devices`)
+    devicesCount.value = response.length
+    for (const device of response) {
+      topicsCount.value += device.device_topics.length
+    }
+  }
 </script>
