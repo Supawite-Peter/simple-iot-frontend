4584fff86e64630420958d07532c65bee7d9001f
Supawite Kiatrattanakorn <supawite08@gmail.com>
2024-11-30 16:13:07 +0700

    Add device id to topic drawer store

--- src/stores/topics-drawer.ts
+++ src/stores/topics-drawer.ts
@@ -3,11 +3,13 @@ import { defineStore } from 'pinia'
 export const useTopicDrawerStore = defineStore('topics-drawer', {
   state: () => ({
     show: false,
+    deviceId: 0,
     deviceName: 'Device Name',
     topic: 'Topic',
   }),
   actions: {
-    openDrawer (deviceName: string, topic: string) {
+    openDrawer (deviceId: number, deviceName: string, topic: string) {
+      this.deviceId = deviceId
       this.deviceName = deviceName
       this.topic = topic
       this.show = true
